d44e48ae743d774b9882c0c0d5998297399701d3
henrique-quintino-auth0 <85711300+henrique-quintino-auth0@users.noreply.github.com>
2021-07-22 17:22:22 -0300

    auth0 added icon

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,12 +1,15 @@
 <template>
   <div class="text-center hero">
     <img class="mb-3 app-logo" src="/logo.png" alt="Vue.js logo" width="120" />
+    <img class="mb-3 app-logo" src="/icon-auth0.png" alt="Auth0 logonp" width="120" />
     <h1 class="mb-4">Vue.js Sample Project</h1>
     <p class="lead">
       This is a sample application that demonstrates an authentication flow for an SPA, using
       <a
         href="https://vuejs.org"
-      >Vue.js</a>
+      >Vue.js</a> and <a
+        href="https://auth0.com"
+      >Auth0</a>
     </p>
   </div>
 </template>
